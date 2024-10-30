ea96ae50d9908b8d02210ad73b801270200cb8e6
Mattias Hemmingsson <matte.hemmingsson@gmail.com>
2024-10-30 07:49:21 +0100

    Updated contentype

--- code/minioBackend.py
+++ code/minioBackend.py
@@ -1,4 +1,5 @@
 from minio import Minio
+from flask import make_response
 import os.path
 
 
@@ -44,6 +45,28 @@ def getFile(filename):
         f = open(FILE_DEST, "rb")
         theFile = f.read()
         f.close()
-        return theFile
+        #set the content type
+        response = make_response(theFile)
+
+        #set the content type
+        filename, file_extension = os.path.splitext(FILE_DEST)
+        if file_extension == ".png":
+            response.headers.set('Content-Type', 'image/png')
+        elif file_extension == ".jpg": 
+            response.headers.set('Content-Type', 'image/jpeg')
+        elif file_extension == ".jpeg":
+            response.headers.set('Content-Type', 'image/jpeg')
+        elif file_extension == ".gif":
+            response.headers.set('Content-Type', 'image/gif')
+        elif file_extension == ".html":
+            response.headers.set('Content-Type', 'text/html')
+        elif file_extension == ".css":
+            response.headers.set('Content-Type', 'text/css')
+        elif file_extension == ".js":
+            response.headers.set('Content-Type', 'text/javascript')
+        elif file_extension == ".json":
+            response.headers.set('Content-Type', 'application/json')
+        return response
+
     else:
         return "File not found"
